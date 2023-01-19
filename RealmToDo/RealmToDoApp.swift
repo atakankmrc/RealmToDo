9976c03ab2e22c0c4bc1e862d0a61f9df4b302a3
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2023-01-19 13:55:47 +0300

    Initial Commit

---
+++ RealmToDo/RealmToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmToDoApp.swift
+//  RealmToDo
+//
+//  Created by Atakan Kömürcü on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RealmToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
